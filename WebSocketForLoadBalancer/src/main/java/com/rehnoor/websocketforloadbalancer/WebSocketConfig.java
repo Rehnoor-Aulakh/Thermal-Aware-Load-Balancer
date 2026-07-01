7b2d82ec5657206a5b847ca6e85aed10ff3f1f4f
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-07-01 11:25:36 +0530

    Removed Unused files

--- WebSocketForLoadBalancer/src/main/java/com/rehnoor/websocketforloadbalancer/WebSocketConfig.java
+++ WebSocketForLoadBalancer/src/main/java/com/rehnoor/websocketforloadbalancer/WebSocketConfig.java
@@ -11,10 +11,12 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private final LoadBalancerWebSocketHandler loadBalancerWebSocketHandler;
 
+    // PARAMETERISED CONSTRUCTOR WITH LoadBalancerWebSocketHandler object
     public WebSocketConfig(LoadBalancerWebSocketHandler loadBalancerWebSocketHandler) {
         this.loadBalancerWebSocketHandler = loadBalancerWebSocketHandler;
     }
-
+    ///  this function registers that whenever someone connects to localhost:8080/lb-server-channel
+    ///  with any origin, then just use loadBalancerWebSocketHandler
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry
